38840f7d165a5c2e992bfc964a1b13b72a30df28
Daniel Huss <spam@unentscheidbar.de>
2023-10-22 15:01:10 +0200

    Bump dependency versions

--- migratedb-gradle-plugin/migratedb-plugin/src/test/groovy/migratedb/v1/gradle/MigrateDbScanTest.groovy
+++ migratedb-gradle-plugin/migratedb-plugin/src/test/groovy/migratedb/v1/gradle/MigrateDbScanTest.groovy
@@ -138,9 +138,10 @@ class MigrateDbScanTest {
         Stream<? extends Arguments> provideArguments(ExtensionContext context) {
             def gradleVersions = [
                 '8.3',
-                '8.2.1', '8.1.1', '8.0.2',
-                '7.6.2', '7.5.1', '7.4.2',
-                '7.3', '7.2', '7.1', '7.0']
+                '8.2.1', // '8.1.1', '8.0.2',
+                '7.6.2', // '7.5.1', '7.4.2',
+                '7.3',   // '7.2', '7.1', '7.0'
+            ]
             def compilingPlugins = [
                 "id 'java'",
                 "id 'java-library'",
